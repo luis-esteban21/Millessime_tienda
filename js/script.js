51987d48c2a6ffafe15a934823c72c212701250b
luis-estena21 <luisnugo@gmail.com>
2021-06-05 10:59:36 -0400

    Version8
    Se agrega js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 
+var nombre = document.getElementById('fname');
+var apellido = document.getElementById('lname');
+var correo = document.getElementById('email');
+var fono = document.getElementById('phone');
+var error = document.getElementById('error');
+error.style = 'red';
 
+function enviarFormulario(){
+console.log('Enviado Formulario....');
+
+var mensajesError = [];
+
+if(nombre.value === null || nombre.value === ''){
+mensajesError.push('Ingresa tu Nombre');
+}
+if(apellido.value === null || apellido.value === ''){
+    mensajesError.push('Ingresa tu Apellido');
+}
+if(correo.value === null || correo.value === ''){
+mensajesError.push('Ingresa tu Email');
+}
+if(fono.value === null || fono.value === ''){
+mensajesError.push('Ingresa tu Telefono');
+}
+
+error.innerHTML = mensajesError.join(', ');
+return false;
+
+}
