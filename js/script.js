0bc6f6c60bef6ec3479f28293d23c36e7eea9b1b
luis-estena21 <luisnugo@gmail.com>
2021-06-05 10:57:09 -0400

    version7

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
-function formatoFecha(fecha, formato) {
-    const map = {
-        dd: fecha.getDate(),
-        mm: fecha.getMonth() + 1,
-        yy: fecha.getFullYear().toString().slice(-2),
-        yyyy: fecha.getFullYear()
-    }
 
-    return formato.replace(/dd|mm|yy|yyy/gi, matched => map[matched])
-}
+
